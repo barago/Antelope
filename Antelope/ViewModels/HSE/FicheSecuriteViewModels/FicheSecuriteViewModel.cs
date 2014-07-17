337ac93345c86224358ee7b5ca73dd32e444c2b8
barago <juco@gmx.fr>
2014-07-18 00:16:27 +0200

    commit

---
+++ Antelope/ViewModels/HSE/FicheSecuriteViewModels/FicheSecuriteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Antelope.ViewModels.HSE.FicheSecuriteViewModels
+{
+    public class FicheSecuriteViewModel
+    {
+    }
+}
