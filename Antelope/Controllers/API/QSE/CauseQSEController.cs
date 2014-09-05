f432577c91fffe7cae57fa5900dd511231f4594d
barago <jucok@gmx.fr>
2014-09-05 17:26:05 +0200

    Tableau des causes Ajout / Edit

---
+++ Antelope/Controllers/API/QSE/CauseQSEController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Antelope.Models;
+using Antelope.Models.QSE;
+
+namespace Antelope.Controllers.API.QSE
+{
+    public class CauseQSEController : ApiController
+    {
+        private AntelopeContext db = new AntelopeContext();
+
+        // GET: api/CauseQSE
+        public IQueryable<CauseQSE> GetCauses()
+        {
+            return db.Causes;
+        }
+
+        // GET: api/CauseQSE/5
+        [ResponseType(typeof(CauseQSE))]
+        public IHttpActionResult GetCauseQSE(int id)
+        {
+            CauseQSE causeQSE = db.Causes.Find(id);
+            if (causeQSE == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(causeQSE);
+        }
+
+        // PUT: api/CauseQSE/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCauseQSE(int id, CauseQSE causeQSE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != causeQSE.CauseQSEId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(causeQSE).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CauseQSEExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CauseQSE
+        //[ResponseType(typeof(CauseQSE))]
+        //public IHttpActionResult PostCauseQSE(CauseQSE causeQSE)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    db.Causes.Add(causeQSE);
+        //    db.SaveChanges();
+
+        //    return CreatedAtRoute("DefaultApi", new { id = causeQSE.CauseQSEId }, causeQSE);
+        //}
+
+        public HttpResponseMessage Post(CauseQSE causeQSE/* [FromBody]string value */)
+        {
+            db.Causes.Add(causeQSE);
+            db.SaveChanges();
+
+            return Request.CreateResponse(HttpStatusCode.OK, Configuration.Formatters.JsonFormatter);
+        }
+
+
+        // DELETE: api/CauseQSE/5
+        [ResponseType(typeof(CauseQSE))]
+        public IHttpActionResult DeleteCauseQSE(int id)
+        {
+            CauseQSE causeQSE = db.Causes.Find(id);
+            if (causeQSE == null)
+            {
+                return NotFound();
+            }
+
+            db.Causes.Remove(causeQSE);
+            db.SaveChanges();
+
+            return Ok(causeQSE);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CauseQSEExists(int id)
+        {
+            return db.Causes.Count(e => e.CauseQSEId == id) > 0;
+        }
+    }
+}
